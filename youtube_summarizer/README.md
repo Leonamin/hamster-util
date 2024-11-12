cd2967a930f960f054a307fa3415acbc5894c188
Leonamin <yism810@gmail.com>
2024-11-12 15:29:28 +0900

    # ADD - YoutubeSummarizer 설정

--- youtube_summarizer/README.md
+++ youtube_summarizer/README.md
@@ -1,5 +1,18 @@
-.env를 만들어 OPENAI_API_KEY를 넣어줍니다
+
+## 환경 설정
+### .env 설정하기
+.env를 만들어 **OPENAI_API_KEY**를 넣어줍니다
 
 ```
 OPENAI_API_KEY=sk-proj-wallahwallahwallahwallahwallahwallahwallahwallahwallahwallahwallahwallah
 ```
+
+### instruction.txt 설정하기
+본인이 원하는 형식을 설정해 줍니다.
+
+### config.json 설정하기
+`input_file_name`과 `output_file_name`을 설정해줍니다. 파일 이름은 확장자까지 작성해줍니다.
+`input_file_name`은 필수 입력이며 `output_file_name`은 input이 없을 경우 `input_file_name` 뒤에 _output이 추가로 붙습니다
+
+### 파일 제공하기
+input 폴더를 생성하여 input 폴더 하위에 `input_file_name`으로 설정한 파일을 저장합니다.
--- youtube_summarizer/main.py
+++ youtube_summarizer/main.py
@@ -4,6 +4,8 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import json
+
 
 # .env 파일에서 환경 변수 로드
 load_dotenv()
@@ -11,19 +13,21 @@ load_dotenv()
 # OpenAI API 클라이언트 생성
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-instruction = """
-주어진 영상들은 만성질환관리(고혈압, 당뇨병) 환자분들께 전달될 일상생활습관 개선, 운동, 식습관 관련 영상들의 자막 텍스트야.
-아래의 조건에 맞게 내용을 요약해줘.
-조건: 핵심 정보만 남기기: 건강 주제와 관련이 없는 내용은 모두 제외해줘.
-- 오탈자 및 띄어쓰기 수정: 문법적 오류나 오탈자는 모두 올바르게 수정하고, 자연스럽고 매끄러운 문장으로 고쳐줘.
-- 쉬운 표현 사용: 어려운 용어나 기술적인 표현은 피하고, 어르신들이 쉽게 이해하실 수 있는 일상적인 말로 바꿔줘.
-- 군더더기 제거: 처음 시작이 안녕하세요, 어르신 이런 부가적인 인사로 시작하는 부분은 제외해줘
-- 길이는 꼭 150자 이내로 마크다운이 아닌 평문으로 넷플릭스 영상 소개 같은 느낌으로 정보를 요약해서 전달해줘.
-텍스트:
+# txt 파일에서 instruction 로드
+with open("instruction.txt", "r", encoding="utf-8") as file:
+    instruction = file.read()
+    if instruction == "":
+        exit("Instruction이 비어있습니다. instruction.txt 파일을 확인해주세요.")
 
-"""
-
-# 나머지 함수들 (is_correct_url, extract_video_id 등)은 그대로 유지
+# json 파일에서 input , output 파일 이름 로드
+with open("config.json", "r", encoding="utf-8") as file:
+    config = json.load(file)
+    input_file_name = config.get("input_file_name")
+    output_file_name = config.get("output_file_name")
+    if input_file_name == "":
+        exit("input_file_name 이 비어있습니다. config.json 파일을 확인해주세요.")
+    if output_file_name == "":
+        f'{input_file_name.split(".")[0]}_output.csv'
 
 
 def is_correct_url(url: str) -> bool:
@@ -69,12 +73,12 @@ def summarize_transcript(transcript):
 
 
 if __name__ == "__main__":
-    input_file_name = 'youtube_links.csv'
-    output_file_name = f'{input_file_name.split(".")[0]}_output.csv'
+    input_file_path = os.path.join('input', input_file_name)
+    output_file_path = os.path.join('output', output_file_name)
 
     try:
-        with open(input_file_name, mode='r', encoding='utf-8') as input_file, \
-                open(output_file_name, mode='w', encoding='utf-8', newline='') as output_file:
+        with open(input_file_path, mode='r', encoding='utf-8') as input_file, \
+                open(output_file_path, mode='w', encoding='utf-8', newline='') as output_file:
             reader = csv.reader(input_file)
             writer = csv.writer(output_file)
 
@@ -108,12 +112,12 @@ if __name__ == "__main__":
                 row[2] = full_transcript
 
                 # GPT 요약 추가
-                summary = summarize_transcript(full_transcript)
-                print("Summary: " + summary)
-                if summary is not None:
-                    row[3] = summary
-                else:
-                    row[3] = "요약 실패"
+                # summary = summarize_transcript(full_transcript)
+                # print("Summary: " + summary)
+                # if summary is not None:
+                #     row[3] = summary
+                # else:
+                #     row[3] = "요약 실패"
 
                 # 결과 파일에 쓰기
                 writer.writerow(row)
